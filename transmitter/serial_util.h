5ba7852e244d7d472364027dafe42bb62350183a
Michael Yenik <mgyenik@ncsu.edu>
2015-11-04 01:49:31 -0800

    Several fixes.
    Many bugs fixed in printing floating point values, namely an included floating
    point formatting routine (which I am *not* very confident in) since the teensy
    standard library dtostrf is totally broken. Also some retabbing was done for
    consistent style. Here are a few seconds of log data while the breadboard I
    have been testing with is being rotated:
    
    BAROMETER -48.25983 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 3627
    ACCELEROMETER: X: 0.03922 Y: 0.82375 Z: 10.59118  m/s^2
    GYROSCOPE: X: 0.01618 Y: 0.02306 Z: 0.02382  rad/s
    MAGNETOMETER: X: 13.18181 Y: 4.81818 Z: -54.18367  uT
    BAROMETER -47.92849 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 3866
    ACCELEROMETER: X: 0.03922 Y: 0.86298 Z: 10.51272  m/s^2
    GYROSCOPE: X: 0.01802 Y: 0.02504 Z: 0.02275  rad/s
    MAGNETOMETER: X: 12.90909 Y: 4.90909 Z: -53.97959  uT
    BAROMETER -48.59116 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 4106
    ACCELEROMETER: X: 0.07845 Y: 0.82375 Z: 10.55195  m/s^2
    GYROSCOPE: X: 0.01603 Y: 0.02412 Z: 0.02687  rad/s
    MAGNETOMETER: X: 13.00000 Y: 4.72727 Z: -54.38775  uT
    BAROMETER -48.09466 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 277
    ACCELEROMETER: X: 0.00000 Y: 0.74530 Z: 10.51272  m/s^2
    GYROSCOPE: X: 0.00519 Y: 0.01756 Z: 0.02565  rad/s
    MAGNETOMETER: X: 13.18181 Y: 5.00000 Z: -53.97959  uT
    BAROMETER -48.67424 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 515
    ACCELEROMETER: X: 0.00000 Y: 0.19613 Z: 10.94422  m/s^2
    GYROSCOPE: X: 0.32284 Y: 0.18325 Z: 0.37614  rad/s
    MAGNETOMETER: X: 13.00000 Y: 4.00000 Z: -54.48979  uT
    BAROMETER -48.50908 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 754
    ACCELEROMETER: X: 0.82375 Y: 1.41215 Z: 11.21880  m/s^2
    GYROSCOPE: X: 0.55206 Y: 0.63942 Z: 0.36132  rad/s
    MAGNETOMETER: X: 4.81818 Y: 6.90909 Z: -56.22448  uT
    BAROMETER -48.09466 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 991
    ACCELEROMETER: X: 2.54972 Y: -2.07901 Z: 10.74808  m/s^2
    GYROSCOPE: X: 0.63087 Y: 0.73013 Z: -2.40100  rad/s
    MAGNETOMETER: X: 4.36363 Y: 10.54545 Z: -56.12244  uT
    BAROMETER -48.42600 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 278
    ACCELEROMETER: X: 0.66685 Y: 0.11767 Z: 10.90499  m/s^2
    GYROSCOPE: X: 0.08078 Y: 0.75625 Z: -1.90040  rad/s
    MAGNETOMETER: X: 8.90909 Y: 5.81818 Z: -57.85714  uT
    BAROMETER -48.17775 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 516
    ACCELEROMETER: X: -1.80442 Y: 0.94143 Z: 10.59118  m/s^2
    GYROSCOPE: X: 0.26099 Y: 0.18478 Z: 2.66520  rad/s
    MAGNETOMETER: X: 9.90909 Y: 4.63636 Z: -58.46938  uT
    BAROMETER -48.34291 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 755
    ACCELEROMETER: X: 0.47071 Y: 1.05911 Z: 10.04201  m/s^2
    GYROSCOPE: X: 0.35170 Y: 0.04138 Z: 0.37736  rad/s
    MAGNETOMETER: X: 12.63636 Y: 4.54545 Z: -57.44897  uT
    BAROMETER -48.25983 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 993
    ACCELEROMETER: X: -1.33370 Y: 0.27458 Z: 10.51272  m/s^2
    GYROSCOPE: X: 0.63652 Y: 0.38606 Z: 0.45967  rad/s
    MAGNETOMETER: X: 17.36363 Y: 13.00000 Z: -51.93877  uT
    BAROMETER -48.25983 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 35
    ACCELEROMETER: X: 0.78453 Y: 0.27458 Z: 10.70886  m/s^2
    GYROSCOPE: X: 0.19593 Y: 0.41309 Z: 0.22235  rad/s
    MAGNETOMETER: X: 13.90909 Y: 10.45454 Z: -53.16326  uT
    BAROMETER -47.92849 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 275
    ACCELEROMETER: X: 0.03922 Y: -3.09890 Z: 18.47572  m/s^2
    GYROSCOPE: X: 0.38866 Y: 0.04016 Z: 0.17302  rad/s
    MAGNETOMETER: X: 11.54545 Y: 6.90909 Z: -54.28571  uT
    BAROMETER -48.34291 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 514
    ACCELEROMETER: X: 0.27458 Y: 1.29447 Z: 9.61051  m/s^2
    GYROSCOPE: X: 0.01542 Y: 0.03222 Z: 0.10552  rad/s
    MAGNETOMETER: X: 12.00000 Y: 4.63636 Z: -53.57142  uT
    BAROMETER -48.34291 ft
    GPS: LAT: 47.71232, LON: -122.20742 AGE: 752
    ACCELEROMETER: X: 0.07845 Y: 0.90221 Z: 10.43427  m/s^2
    GYROSCOPE: X: 0.01542 Y: 0.02306 Z: 0.02351  rad/s
    MAGNETOMETER: X: 12.00000 Y: 4.54545 Z: -53.67346  uT
    BAROMETER -48.25983 ft

--- transmitter/serial_util.h
+++ transmitter/serial_util.h
@@ -15,15 +15,15 @@ static void pprintf(Print *p, T fmt, ... );
 // Specialized for string literals.
 template<>
 void pprintf(Print *p, const char *fmt, ... ) {
-	// Sting truncated at 128 chars, can be increased if stack depth not
-	// concern.
-	char buf[128];
-	va_list args;
+  // Sting truncated at 128 chars, can be increased if stack depth not
+  // concern.
+  char buf[128];
+  va_list args;
 
-	va_start(args, fmt );
-	vsnprintf(buf, 128, fmt, args);
-	va_end(args);
-	p->print(buf);
+  va_start(args, fmt );
+  vsnprintf(buf, 128, fmt, args);
+  va_end(args);
+  p->print(buf);
 }
 
 // Map from sensor type to a static string.
@@ -37,51 +37,51 @@ const char *sensorTypeUnits(const sensor_t& sensor);
 // redundancy in printing.
 class SensorWriter {
  public:
-	SensorWriter() {}
-	virtual ~SensorWriter() {}
+  SensorWriter() {}
+  virtual ~SensorWriter() {}
 
-	// Take sample from sensor so that all logging sources get the same value.
-	virtual void Update() = 0;
+  // Take sample from sensor so that all logging sources get the same value.
+  virtual void Update() = 0;
 
- 	// Log this sample to the given stream.
-	virtual void Write(Print *p) = 0;
+  // Log this sample to the given stream.
+  virtual void Write(Print *p) = 0;
 };
 
 class UnifiedSensorWriter : public SensorWriter {
  public:
-	UnifiedSensorWriter(Adafruit_Sensor *sensor);
-	void Update();
- 	void Write(Print *p);
+  UnifiedSensorWriter(Adafruit_Sensor *sensor);
+  void Update();
+  void Write(Print *p);
 
  private:
-	Adafruit_Sensor *sensor_;
-	sensors_event_t event_;
-	const char *name_;
-	const char *unit_;
+  Adafruit_Sensor *sensor_;
+  sensors_event_t event_;
+  const char *name_;
+  const char *unit_;
 };
 
 class AltitudeSensorWriter : public SensorWriter {
  public:
-	AltitudeSensorWriter(Adafruit_BMP085_Unified *bmp);
-	void Update();
- 	void Write(Print *p);
+  AltitudeSensorWriter(Adafruit_BMP085_Unified *bmp);
+  void Update();
+  void Write(Print *p);
 
  private:
-	Adafruit_BMP085_Unified *bmp_;
-	sensors_event_t event_;
-	const char *name_;
-	const char *unit_;
+  Adafruit_BMP085_Unified *bmp_;
+  sensors_event_t event_;
+  const char *name_;
+  const char *unit_;
 };
 
 class GPSSensorWriter : public SensorWriter {
  public:
-	GPSSensorWriter(TinyGPS *gps, Stream *s) : gps_(gps), stream_(s) {}
-	void Update();
- 	void Write(Print *p);
+  GPSSensorWriter(TinyGPS *gps, Stream *s) : gps_(gps), stream_(s) {}
+  void Update();
+  void Write(Print *p);
 
  private:
-	TinyGPS *gps_;
-	Stream *stream_;
+  TinyGPS *gps_;
+  Stream *stream_;
 };
 
 void sensorDetailsOut(const sensor_t& sensor, Stream *serial);
